4a8dfdff62cfc8aca58066d4ae0e14bb52b51dc5
Phwxx <wwbphx@gmail.com>
2019-12-20 15:17:27 +0800

    first ver.

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+// $(function () {
+//   $(document).on('ready', function() { 
+//   $('.slick-reviews').slick({
+//     slidesToShow: 1,
+//     slidesToScroll: 1,
+//     autoplay: true,
+//     autoplaySpeed: 4000,
+//     dots: true,
+//     arrows: false
+//   });
+// }
